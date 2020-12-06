b367f27ec973b4a179026e0e2886dc85ffb5a8c4
cannatx <nachocane@msn.com>
2020-12-06 10:04:13 +0100

    script de prueba para subir

---
+++ busca.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+
+find_str = input('Que quieres buscar?\n')
+# find_str = 'Video'
+dir_to_look = './__ENTEL__/'
+dest = './__02_SALIDA_ORDENADOR/'
+
+def busca(path,look):
+    res = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if look.lower() in file.lower():
+                res.append(os.path.join(root,file))
+    return res
+
+def mueve(files,dest):
+    for file in files:
+        cmd = 'mv -vf "{}" "{}"'.format(file,dest)
+        os.system(cmd)
+
+def main():
+    archivos = busca(dir_to_look,find_str)
+    if archivos == []:
+        res = 'NO ENCONTRADO'
+        print(res)
+        return res
+    else:
+        mueve(archivos,dest)
+
+if __name__ == "__main__":
+    main()
